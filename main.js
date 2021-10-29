c2a7021e9d2f6f78a4ff84a7d79f436826e0b5dd
SHOR00 <jorgedrios86@gmail.com>
2021-10-29 20:07:49 -0300

    nuevo

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.contenedor');
+
+ScrollReveal().reveal('.navbar', {delay: 500});
+ScrollReveal().reveal('h1', {delay: 500});
+
+
+ScrollReveal().reveal('.contenido-dos', {delay: 400});
+
+ScrollReveal().reveal('.rph', {delay: 600});
+
+ScrollReveal().reveal('.rphTexto', {delay: 1000});
+
