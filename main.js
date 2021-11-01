03c5ec71972d5705708739e87febe46f0304b0cc
SHOR00 <jorgedrios86@gmail.com>
2021-11-01 20:20:25 -0300

    actual

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+// scroll
+
 ScrollReveal().reveal('.contenedor');
 
 ScrollReveal().reveal('.navbar', {delay: 500});
@@ -9,4 +12,30 @@ ScrollReveal().reveal('.contenido-dos', {delay: 400});
 ScrollReveal().reveal('.rph', {delay: 600});
 
 ScrollReveal().reveal('.rphTexto', {delay: 1000});
+
+
+// funcion slider
+
+
+
+const arrayImagenes = ["img/repairman-doing-air-conditioner-service.jpg", "img/air-conditioning-decoration-interior.jpg", "img/IMG-20211007-WA0065.jpg", "img/IMG-20211007-WA0061.jpg"];
+
+const arrayTextos = ["Servicio Técnico","Instalaciones Domiciliarias", "Mantenimiento", "Refrigeración Automotores"];
+
+let num = 0;
+
+function slider() {
+    const contenidoFlex = document.querySelector(".contenido-flex");
+    const parrafo = document.querySelector(".parrafo")
+    if (num >= arrayImagenes.length) {
+        num = 0
+    }
+
+
+    contenidoFlex.style = `background: url(${arrayImagenes[num]}) no-repeat center center/cover`;
+    parrafo.innerHTML = arrayTextos[num];
+    num++
+};
+
+    setInterval(slider, 4000);
 
